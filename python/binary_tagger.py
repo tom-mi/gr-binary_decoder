7fa289110d546a6a477e294ff07ea5856b30050c
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-12-23 18:59:34 +0100

    Implement basic tagging

--- python/binary_tagger.py
+++ python/binary_tagger.py
@@ -22,22 +22,44 @@
 
 import numpy
 from gnuradio import gr
+import pmt
+
 
 class binary_tagger(gr.sync_block):
     """
     docstring for block binary_tagger
     """
-    def __init__(self, samples_per_symbol=1, max_quiet_symbols=10):
-        gr.sync_block.__init__(self,
-            name="binary_tagger",
-            in_sig=[<+numpy.float32+>, ],
-            out_sig=[<+numpy.float32+>, ])
 
+    def __init__(self, key='binary_transmission', max_quiet_samples=100):
+        gr.sync_block.__init__(self,
+                               name="binary_tagger",
+                               in_sig=[numpy.int8, ],
+                               out_sig=[numpy.int8, ])
+
+        self._key = key
+        self._max_quiet_samples = max_quiet_samples
+        self._is_transmission = False
+        self._position_of_last_signal = -1
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
-        # <+signal processing here+>
-        out[:] = in0
-        return len(output_items[0])
 
+        offset = self.nitems_written(0)
+
+        for i, value in enumerate(in0):
+            position = offset + i
+            if value != 0:
+                self._position_of_last_signal = position
+            if not self._is_transmission:
+                if value != 0:
+                    self.add_item_tag(0, position, pmt.string_to_symbol(self._key), pmt.to_pmt(True))
+                    self._is_transmission = True
+            else:
+                if position - self._position_of_last_signal > self._max_quiet_samples:
+                    self._is_transmission = False
+                    self.add_item_tag(0, position, pmt.string_to_symbol(self._key), pmt.to_pmt(False))
+
+        out[:] = in0
+
+        return len(output_items[0])
--- python/qa_binary_tagger.py
+++ python/qa_binary_tagger.py
@@ -18,10 +18,22 @@
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
 #
+from dataclasses import dataclass
 
+import pmt
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 from binary_tagger import binary_tagger
+
+TEST_KEY = 'test_key'
+
+
+@dataclass
+class ExpectedTag(object):
+    offset: int
+    key: str
+    value: object
+
 
 class qa_binary_tagger(gr_unittest.TestCase):
 
@@ -31,10 +43,109 @@ class qa_binary_tagger(gr_unittest.TestCase):
     def tearDown(self):
         self.tb = None
 
-    def test_001_t(self):
-        # set up fg
+    def test_zeroes_only_yield_no_tags(self):
+        # given
+        data = (0, 0, 0, 0, 0)
+        self._setup_graph(data)
+
+        # when
         self.tb.run()
-        # check data
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags(())
+
+    def test_yields_starting_tag(self):
+        # given
+        data = (0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1)
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags((
+            ExpectedTag(2, TEST_KEY, True),
+        ))
+
+    def test_yields_starting_and_ending_tag(self):
+        # given
+        data = (0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0)
+        self._setup_graph(data, max_quiet_samples=4)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags((
+            ExpectedTag(2, TEST_KEY, True),
+            ExpectedTag(14, TEST_KEY, False),
+        ))
+
+    def test_yields_multiple_transmissions(self):
+        # given
+        data = (0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0)
+        self._setup_graph(data, max_quiet_samples=2)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags((
+            ExpectedTag(2, TEST_KEY, True),
+            ExpectedTag(7, TEST_KEY, False),
+            ExpectedTag(9, TEST_KEY, True),
+            ExpectedTag(15, TEST_KEY, False),
+            ExpectedTag(16, TEST_KEY, True),
+            ExpectedTag(19, TEST_KEY, False),
+        ))
+
+    def test_yields_transmissions_for_large_number_of_samples(self):
+        # given
+        data = (0,) * 100_000 + (1, 0, 0, 1, 0, 0, 1, 1) + (0,) * 100_000
+        self._setup_graph(data, max_quiet_samples=10)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags((
+            ExpectedTag(100_000, TEST_KEY, True),
+            ExpectedTag(100_018, TEST_KEY, False),
+        ))
+
+    def test_yields_transmissions_for_large_number_of_samples_with_long_transmission(self):
+        # given
+        data = (0,) * 100_000 + (1, 0, 0, 1, 0, 0, 1, 1) * 100_00 + (0,) * 11
+        self._setup_graph(data, max_quiet_samples=10)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), data)
+        self._assert_tags((
+            ExpectedTag(100_000, TEST_KEY, True),
+            ExpectedTag(180_010, TEST_KEY, False),
+        ))
+
+    def _setup_graph(self, src_data, max_quiet_samples=100):
+        src = blocks.vector_source_b(src_data)
+        uut = binary_tagger(key=TEST_KEY, max_quiet_samples=max_quiet_samples)
+        self.dst = blocks.vector_sink_b()
+        self.tb.connect(src, uut)
+        self.tb.connect(uut, self.dst)
+
+    def _assert_tags(self, tags: [ExpectedTag]):
+        self.assertEqual(len(self.dst.tags()), len(tags))
+        for tag, expected_tag in zip(self.dst.tags(), tags):
+            self.assertEqual(tag.offset, expected_tag.offset)
+            self.assertEqual(pmt.symbol_to_string(tag.key), expected_tag.key)
+            self.assertEqual(pmt.to_python(tag.value), expected_tag.value)
 
 
 if __name__ == '__main__':
