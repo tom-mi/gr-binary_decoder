56aa21a8a49593e21c16c4d3b5e9f1c2d4101a56
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-12-23 22:23:31 +0100

    Extract more common test code

--- python/qa_binary_tagger.py
+++ python/qa_binary_tagger.py
@@ -19,22 +19,14 @@
 # Boston, MA 02110-1301, USA.
 #
 
-import pmt
-from gnuradio import gr, gr_unittest
-from gnuradio import blocks
+from gnuradio import gr_unittest
 from binary_tagger import binary_tagger
-from qa_common import ExpectedTag
+from qa_common import ExpectedTag, BinaryBaseTest
 
 TEST_KEY = 'test_key'
 
 
-class qa_binary_tagger(gr_unittest.TestCase):
-
-    def setUp(self):
-        self.tb = gr.top_block()
-
-    def tearDown(self):
-        self.tb = None
+class qa_binary_tagger(BinaryBaseTest):
 
     def test_zeroes_only_yield_no_tags(self):
         # given
@@ -127,18 +119,8 @@ class qa_binary_tagger(gr_unittest.TestCase):
         ))
 
     def _setup_graph(self, src_data, max_quiet_samples=100):
-        src = blocks.vector_source_b(src_data)
         uut = binary_tagger(key=TEST_KEY, max_quiet_samples=max_quiet_samples)
-        self.dst = blocks.vector_sink_b()
-        self.tb.connect(src, uut)
-        self.tb.connect(uut, self.dst)
-
-    def _assert_tags(self, tags: [ExpectedTag]):
-        self.assertEqual(len(self.dst.tags()), len(tags))
-        for tag, expected_tag in zip(self.dst.tags(), tags):
-            self.assertEqual(tag.offset, expected_tag.offset)
-            self.assertEqual(pmt.symbol_to_string(tag.key), expected_tag.key)
-            self.assertEqual(pmt.to_python(tag.value), expected_tag.value)
+        self._setup_graph_with_uut(src_data, uut)
 
 
 if __name__ == '__main__':
--- python/qa_common.py
+++ python/qa_common.py
@@ -1,8 +1,35 @@
 from dataclasses import dataclass
 
+import pmt
+from gnuradio import gr, gr_unittest, blocks
+
+TEST_KEY = 'test_key'
+
 
 @dataclass
 class ExpectedTag(object):
     offset: int
     key: str
     value: object
+
+
+class BinaryBaseTest(gr_unittest.TestCase):
+
+    def setUp(self):
+        self.tb = gr.top_block()
+
+    def tearDown(self):
+        self.tb = None
+
+    def _setup_graph_with_uut(self, src_data, uut):
+        src = blocks.vector_source_b(src_data)
+        self.dst = blocks.vector_sink_b()
+        self.tb.connect(src, uut)
+        self.tb.connect(uut, self.dst)
+
+    def _assert_tags(self, tags: [ExpectedTag]):
+        self.assertEqual(len(self.dst.tags()), len(tags))
+        for tag, expected_tag in zip(self.dst.tags(), tags):
+            self.assertEqual(tag.offset, expected_tag.offset)
+            self.assertEqual(pmt.symbol_to_string(tag.key), expected_tag.key)
+            self.assertEqual(pmt.to_python(tag.value), expected_tag.value)
