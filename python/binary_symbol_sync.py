aff8b1b84b6e81e8d306a87a52d50fed3348529c
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-12-26 11:46:29 +0100

    Initial implementation of binary symbol sync

--- python/binary_symbol_sync.py
+++ python/binary_symbol_sync.py
@@ -23,22 +23,102 @@
 import numpy
 from gnuradio import gr
 
+
 class binary_symbol_sync(gr.basic_block):
     """
     docstring for block binary_symbol_sync
     """
+
     def __init__(self, samples_per_symbol=10, max_deviation=2, max_zero_symbols=10, output_samples_per_symbol=1):
         gr.basic_block.__init__(self,
-            name="binary_symbol_sync",
-            in_sig=[<+numpy.float32+>, ],
-            out_sig=[<+numpy.float32+>, ])
+                                name="binary_symbol_sync",
+                                in_sig=[numpy.int8, ],
+                                out_sig=[numpy.int8, ])
+        self._samples_per_symbol = samples_per_symbol
+        self._max_deviation = max_deviation
+        self._max_zero_symbols = max_zero_symbols
+        self._output_samples_per_symbol = output_samples_per_symbol
+
+        self.set_output_multiple(self._output_samples_per_symbol)
+
+        # TODO check validity of deviation
+        self._min_samples_per_symbol = self._samples_per_symbol - self._max_deviation
+        self._max_samples_per_symbol = self._samples_per_symbol + self._max_deviation
+
+        # internal block state
+        self._is_locked = False
+        self._current_samples_per_symbol = None
+        self._zero_symbols = 0
 
     def forecast(self, noutput_items, ninput_items_required):
-        #setup size of input_items[i] for work call
+        # setup size of input_items[i] for work call
         for i in range(len(ninput_items_required)):
-            ninput_items_required[i] = noutput_items
+            ninput_items_required[i] = int(
+                noutput_items / self._output_samples_per_symbol) * self._max_samples_per_symbol + 1
 
     def general_work(self, input_items, output_items):
-        output_items[0][:] = input_items[0]
-        consume(0, len(input_items[0]))        #self.consume_each(len(input_items[0]))
-        return len(output_items[0])
+        in0 = input_items[0]
+        out0 = output_items[0]
+
+        relative_position = 0
+        symbols_written = 0
+
+        def _skip_empty():
+            if numpy.all(in0[relative_position:] == 0):
+                return len(in0)
+            return numpy.argmax(in0[relative_position:] != 0)
+
+        def _lock():
+            self._is_locked = True
+            self._current_samples_per_symbol = self._samples_per_symbol
+
+        def _determine_current_symbol_length():
+            best_symbol_length = None
+            best_error = self._max_deviation * 2
+            for i in range(self._min_samples_per_symbol, self._max_samples_per_symbol + 1):
+                if in0[relative_position + i-1] == 0 and in0[relative_position + i] != 0:
+                    error = numpy.abs(i - self._current_samples_per_symbol)
+                    print(f'... i={i} error={error}')
+                    if error < best_error:
+                        best_error = error
+                        best_symbol_length = i
+            if best_symbol_length is not None:
+                self._current_samples_per_symbol = (self._current_samples_per_symbol + best_symbol_length) / 2
+                print(f'found i={best_symbol_length}, avg={self._current_samples_per_symbol}')
+                return best_symbol_length
+            return int(self._current_samples_per_symbol)
+
+        def _send_symbol(length):
+            if numpy.any(in0[relative_position:relative_position + length] != 0):
+                self._zero_symbols = 0
+            else:
+                self._zero_symbols += 1
+            symbol_offset = symbols_written * self._output_samples_per_symbol
+            for i in range(self._output_samples_per_symbol):
+                out0[symbol_offset + i] = in0[relative_position + int(length * i / self._output_samples_per_symbol)]
+
+        print(f'pos={relative_position} len(in0)={len(in0)} len(out0)={len(out0)}')
+        while (relative_position + self._max_samples_per_symbol < len(in0)
+               and (symbols_written + 1) * self._output_samples_per_symbol <= len(out0)):
+            if not self._is_locked:
+                if in0[relative_position] == 0:
+                    relative_position = _skip_empty()
+                    print(f'skipping to pos={relative_position}')
+                else:
+                    _lock()
+            else:
+                if self._zero_symbols > self._max_zero_symbols:
+                    self._is_locked = False
+                    self._zero_symbols = 0
+                else:
+                    length = _determine_current_symbol_length()
+                    print(f'locked, length={length}')
+                    _send_symbol(length)
+                    symbols_written += 1
+                    relative_position += length
+
+        # output_items[0][:] = input_items[0]
+        self.consume(0, relative_position)  # self.consume_each(len(input_items[0]))
+        print(f'consumed {relative_position} samples, symbols written: {symbols_written}')
+        print('wrote samples', symbols_written * self._output_samples_per_symbol)
+        return symbols_written * self._output_samples_per_symbol
--- python/qa_binary_symbol_sync.py
+++ python/qa_binary_symbol_sync.py
@@ -19,22 +19,124 @@
 # Boston, MA 02110-1301, USA.
 #
 
-from gnuradio import gr, gr_unittest
-from gnuradio import blocks
+from gnuradio import gr_unittest
+
 from binary_symbol_sync import binary_symbol_sync
+from qa_common import BinaryBaseTest
 
-class qa_binary_symbol_sync(gr_unittest.TestCase):
 
-    def setUp(self):
-        self.tb = gr.top_block()
+class qa_binary_symbol_sync(BinaryBaseTest):
 
-    def tearDown(self):
-        self.tb = None
+    def test_zeroes_only_yield_no_output(self):
+        # given
+        data = (0,) * 20
+        self._setup_graph(data)
 
-    def test_001_t(self):
-        # set up fg
+        # when
         self.tb.run()
-        # check data
+
+        # then
+        self.assertEqual(self.dst.data(), ())
+
+    def test_returns_single_symbol(self):
+        # given
+        data = (1, 1, 0, 0, 0) + (0,)
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=0)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1,))
+
+    def test_returns_multiple_symbols(self):
+        # given
+        data = (1, 1, 0, 0, 0) * 2 + (0, 0, 0, 0, 0) * 2 + (1, 0, 1, 0, 0) + (0,)
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=0)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 0, 0, 1))
+
+    def test_returns_multiple_symbols_with_leading_zeros(self):
+        # given
+        data = (0,) * 13 + (1, 1, 0, 0, 0) * 2 + (0, 0, 0, 0, 0) * 2 + (1, 0, 1, 0, 0) + (0,)
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=0)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 0, 0, 1))
+
+    def test_returns_multiple_symbols_with_trailing_zeros_but_loses_lock_after_more_than_max_zero_symbols(self):
+        # given
+        data = (1, 1, 0, 0, 0) * 2 + (0, 0, 0, 0, 0) * 2 + (1, 0, 1, 0, 0) + (0,) * 5 * 4
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=0, max_zero_symbols=2)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 0, 0, 1, 0, 0, 0))
+
+    def test_interpolates_output_samples(self):
+        # given
+        data = (1, 1, 0, 0, 0) + (0, 0, 0, 0, 0) + (1, 0, 1, 0, 0) + (0,)
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=0, max_zero_symbols=2, output_samples_per_symbol=3)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 0) + (0, 0, 0) + (1, 0, 0))
+
+    def test_handles_unstable_clock(self):
+        # given
+        data = (1, 1, 0, 0, 0) + (0, 0, 0, 0, 0) + (1, 0, 1, 0, 0, 0, 0) + (1, 1, 0) + (1, 1, 0, 1, 0) + (0,) * 30
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=2, max_zero_symbols=2, output_samples_per_symbol=2)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 0) + (0, 0) + (1, 0) + (1, 1) + (1, 0) + (0, 0) * 3)
+
+    def test_handles_lock_to_drifted_clock(self):
+        # given
+        data = (1, 1, 0, 0, 0, 0, 0, 0) * 4 + (0, 0, 0, 0, 0, 0, 0, 0) * 4 + (1, 1, 0, 0, 0, 0, 0, 0) * 2 + (0,) * 10
+        self._setup_graph(data, samples_per_symbol=10, max_deviation=3, max_zero_symbols=4, output_samples_per_symbol=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 1, 1, 0, 0, 0, 0, 1, 1))
+
+    def test_handles_multiple_transmissions(self):
+        # given
+        one = (1, 1, 0, 0, 0)
+        zero = (0, 0, 0, 0, 0)
+        data = one * 3 + zero + one + zero * 5 + one * 3 + one + one + zero * 4
+        self._setup_graph(data, samples_per_symbol=5, max_deviation=1, max_zero_symbols=2, output_samples_per_symbol=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0))
+
+    def _setup_graph(self, src_data, samples_per_symbol=10, max_deviation=2, max_zero_symbols=5,
+                     output_samples_per_symbol=1):
+        uut = binary_symbol_sync(
+            samples_per_symbol=samples_per_symbol,
+            max_deviation=max_deviation,
+            max_zero_symbols=max_zero_symbols,
+            output_samples_per_symbol=output_samples_per_symbol,
+        )
+        self._setup_graph_with_uut(src_data, uut)
 
 
 if __name__ == '__main__':
