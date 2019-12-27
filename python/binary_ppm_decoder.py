859f83e49f3ceeec7c269ee2d9a43d8d92edc33e
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2019-12-27 02:57:08 +0100

    Implement binary ppm decoder

--- python/binary_ppm_decoder.py
+++ python/binary_ppm_decoder.py
@@ -18,27 +18,103 @@
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
 #
-
+import enum
+import itertools
 
 import numpy
+import typing
 from gnuradio import gr
+
 
 class binary_ppm_decoder(gr.basic_block):
     """
     docstring for block binary_ppm_decoder
     """
-    def __init__(self, samples_per_peak=10, samples_per_gap=[10, 20], max_deviation=1):
+
+    def __init__(self, samples_per_pulse=10, samples_per_gap=(10, 20), max_deviation=1):
         gr.basic_block.__init__(self,
-            name="binary_ppm_decoder",
-            in_sig=[<+numpy.float32+>, ],
-            out_sig=[<+numpy.float32+>, ])
+                                name="binary_ppm_decoder",
+                                in_sig=[numpy.int8, ],
+                                out_sig=[numpy.int8, ])
+        self._samples_per_pulse = samples_per_pulse
+        self._samples_per_gap = numpy.array(list(samples_per_gap))
+        self._max_deviation = max_deviation
+        self._validate_parameters()
+
+        # internal state
+        self._last_positive_edge = None
+        self._last_negative_edge = None
+        self._pending_symbol = None
+
+    def _validate_parameters(self):
+        if self._samples_per_pulse < 1 or not isinstance(self._samples_per_pulse, int):
+            raise ValueError('samples_per_pulse must be a positive integer')
+        if len(set(self._samples_per_gap)) < len(self._samples_per_gap) or \
+                not issubclass(self._samples_per_gap.dtype.type, numpy.integer) or \
+                numpy.any(self._samples_per_gap < 1):
+            raise ValueError('samples_per_gap must be a list of distinct positive integers')
+        if len(self._samples_per_gap) < 2:
+            raise ValueError('samples_per_gap must have at least two elements')
+        if self._max_deviation < 0 or type(self._max_deviation) != int:
+            raise ValueError('max_deviation must be a non-negative integer')
+        min_gap_distance = min([abs(y - x) for x, y in itertools.combinations(self._samples_per_gap, 2)])
+        if min_gap_distance <= 2 * self._max_deviation:
+            raise ValueError('difference between any 2 values in samples_per_gap must not be smaller '
+                             'than 2 * max_deviation')
 
     def forecast(self, noutput_items, ninput_items_required):
-        #setup size of input_items[i] for work call
+        # setup size of input_items[i] for work call
         for i in range(len(ninput_items_required)):
-            ninput_items_required[i] = noutput_items
+            ninput_items_required[i] = noutput_items * \
+                                       (self._samples_per_pulse + int(numpy.min(self._samples_per_gap)))
 
     def general_work(self, input_items, output_items):
-        output_items[0][:] = input_items[0]
-        consume(0, len(input_items[0]))        #self.consume_each(len(input_items[0]))
-        return len(output_items[0])
+        in0 = input_items[0]
+        out0 = output_items[0]
+
+        normalized_input = numpy.abs(numpy.sign(in0))
+        differential_input = numpy.diff(normalized_input)
+
+        edges = list(numpy.argwhere(differential_input != 0))
+
+        sent_symbols = 0
+        nitems_read = self.nitems_read(0)
+
+        while len(edges) > 0 and sent_symbols < len(out0):
+            edge = edges.pop(0)
+            edge_type = numpy.asscalar(differential_input[edge])
+            if edge_type == 1:
+                self._last_positive_edge = edge + nitems_read
+                if self._last_negative_edge is not None:
+                    gap = self._last_positive_edge - self._last_negative_edge
+                    symbol = self._decode_gap_to_symbol(gap)
+                    if symbol is not None:
+                        self._pending_symbol = symbol
+                    else:
+                        self._pending_symbol = None
+            elif edge_type == -1:
+                self._last_negative_edge = None
+                if self._last_positive_edge is not None:
+                    pulse = edge + nitems_read - self._last_positive_edge
+                    if numpy.abs(pulse - self._samples_per_pulse) <= self._max_deviation:
+                        self._last_negative_edge = edge + nitems_read
+                        if self._pending_symbol is not None:
+                            out0[sent_symbols] = self._pending_symbol
+                            sent_symbols += 1
+                            self._pending_symbol = None
+            else:
+                raise RuntimeError(f'Invalid edge type {edge_type}')
+
+        if len(edges) == 0:
+            consumed = len(in0) - 1
+        else:
+            consumed = edges[0]  # TODO is that a good idea?
+
+        self.consume(0, consumed)
+        return sent_symbols
+
+    def _decode_gap_to_symbol(self, gap):
+        for symbol, expected_samples in enumerate(self._samples_per_gap):
+            if numpy.abs(gap - expected_samples) <= self._max_deviation:
+                return symbol
+        return None
--- python/qa_binary_ppm_decoder.py
+++ python/qa_binary_ppm_decoder.py
@@ -19,22 +19,172 @@
 # Boston, MA 02110-1301, USA.
 #
 
-from gnuradio import gr, gr_unittest
-from gnuradio import blocks
+from gnuradio import gr_unittest
+
 from binary_ppm_decoder import binary_ppm_decoder
+from qa_common import BinaryBaseTest
 
-class qa_binary_ppm_decoder(gr_unittest.TestCase):
+PULSE = (1,) * 3
+SHORT_GAP = (0,) * 5
+LONG_GAP = (0,) * 9
+ZERO = (0,)
+TRANSMISSION_BREAK = (0,) * 15
 
-    def setUp(self):
-        self.tb = gr.top_block()
 
-    def tearDown(self):
-        self.tb = None
+class qa_binary_ppm_decoder(BinaryBaseTest):
 
-    def test_001_t(self):
-        # set up fg
+    def test_invalid_parameters_are_rejected(self):
+        for parameters, message in [
+            ({'samples_per_pulse': -1}, 'samples_per_pulse must be a positive integer'),
+            ({'samples_per_pulse': 0}, 'samples_per_pulse must be a positive integer'),
+            ({'samples_per_pulse': 1.}, 'samples_per_pulse must be a positive integer'),
+            ({'samples_per_gap': 'abc'}, 'samples_per_gap must be a list of distinct positive integers'),
+            ({'samples_per_gap': (1, 1)}, 'samples_per_gap must be a list of distinct positive integers'),
+            ({'samples_per_gap': (1., 2)}, 'samples_per_gap must be a list of distinct positive integers'),
+            ({'samples_per_gap': (0, 2)}, 'samples_per_gap must be a list of distinct positive integers'),
+            ({'samples_per_gap': (-1, 2)}, 'samples_per_gap must be a list of distinct positive integers'),
+            ({'max_deviation': -1}, 'max_deviation must be a non-negative integer'),
+            ({'max_deviation': 0.}, 'max_deviation must be a non-negative integer'),
+            ({'max_deviation': 2, 'samples_per_gap': (3, 14, 7)},
+             'difference between any 2 values in samples_per_gap must not be smaller than 2 * max_deviation'),
+            ({'samples_per_gap': (3,)}, 'samples_per_gap must have at least two elements'),
+        ]:
+            with self.subTest(f'{parameters} -> {message}'):
+                with self.assertRaises(ValueError) as error:
+                    binary_ppm_decoder(**parameters)
+                self.assertEqual(str(error.exception), message)
+
+    def test_zeroes_only_yield_no_output(self):
+        # given
+        data = (0,) * 20
+        self._setup_graph(data)
+
+        # when
         self.tb.run()
-        # check data
+
+        # then
+        self.assertEqual(self.dst.data(), ())
+
+    def test_receive_single_symbol(self):
+        # given
+        data = ZERO + PULSE + SHORT_GAP + PULSE + ZERO
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (0,))
+
+    def test_receive_multiple_symbols(self):
+        # given
+        data = ZERO + PULSE + LONG_GAP + PULSE + SHORT_GAP + PULSE + LONG_GAP + PULSE + ZERO
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 0, 1))
+
+    def test_receive_multiple_symbols_with_more_than_two_symbol_types(self):
+        # given
+        data = ZERO + PULSE + (0,) * 8 + PULSE + (0,) * 5 + PULSE + (0,) + PULSE + ZERO
+        self._setup_graph(data, samples_per_gap=(1, 2, 3, 4, 5, 6, 7, 8))
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (7, 4, 0))
+
+    def test_receive_multiple_symbols_with_pause(self):
+        # given
+        data = ZERO + PULSE + LONG_GAP + PULSE + TRANSMISSION_BREAK + PULSE + SHORT_GAP + PULSE + ZERO
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 0))
+
+    def test_receive_multiple_symbols_with_timing_deviation(self):
+        # given
+        data = ZERO + PULSE + (1,) + LONG_GAP + PULSE + SHORT_GAP + (0,) + PULSE + (0, 0, 0, 0) + PULSE + ZERO
+        self._setup_graph(data, max_deviation=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1, 0, 0))
+
+    def test_ignores_symbol_after_invalid_start_pulse(self):
+        # given
+        data = ZERO + (1,) + LONG_GAP + PULSE + SHORT_GAP + PULSE + ZERO
+        self._setup_graph(data, max_deviation=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (0,))
+
+    def test_ignores_symbol_after_invalid_end_pulse(self):
+        # given
+        data = ZERO + PULSE + LONG_GAP + PULSE + SHORT_GAP + (1,) * 5 + ZERO
+        self._setup_graph(data, max_deviation=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1,))
+
+    def test_ignores_symbol_with_invalid_gap_length(self):
+        # given
+        data = ZERO + PULSE + (0,) * 7 + PULSE + SHORT_GAP + PULSE + ZERO
+        self._setup_graph(data, max_deviation=1)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (0,))
+
+    def test_receive_multiple_symbols_with_large_pause(self):
+        # given
+        data = ZERO + (PULSE + LONG_GAP) * 5 + \
+               PULSE + TRANSMISSION_BREAK * 100_000 + \
+               (PULSE + SHORT_GAP + PULSE + TRANSMISSION_BREAK * 10_000) * 5 + ZERO
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1,) * 5 + (0,) * 5)
+
+    def test_receive_large_number_of_symbols(self):
+        # given
+        data = ZERO + (PULSE + LONG_GAP) * 100_000 + PULSE + ZERO
+        self._setup_graph(data)
+
+        # when
+        self.tb.run()
+
+        # then
+        self.assertEqual(self.dst.data(), (1,) * 100_000)
+
+    def _setup_graph(self, src_data, samples_per_pulse=3, samples_per_gap=(5, 9),
+                     max_deviation=0):
+        uut = binary_ppm_decoder(
+            samples_per_pulse=samples_per_pulse,
+            samples_per_gap=samples_per_gap,
+            max_deviation=max_deviation,
+        )
+        self._setup_graph_with_uut(src_data, uut)
 
 
 if __name__ == '__main__':
